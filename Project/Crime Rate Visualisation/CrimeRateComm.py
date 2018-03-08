0ff92a33d732198b4a420fcd6a3344445ffdcb61
EnazS <zane_seq@outlook.com>
2018-03-08 11:07:03 +0530

    Update CrimeRateComm.py

--- Project/Crime Rate Visualisation/CrimeRateComm.py
+++ Project/Crime Rate Visualisation/CrimeRateComm.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 f = open('/home/zanesx/Desktop/CrimeRateInferenceWithBigData-master/CrimeRateInference_SourceCode/CrimeRateInference_python/data/demographics.csv')
 reader_demo = csv.DictReader(f)
 x = []
-y = []  # y is crime rate vecctor
+y = []  # y is crime rate vector
 flag = 0
 for row in reader_demo:
     x.append(int(row['Community #']))
@@ -13,10 +13,18 @@ for row in reader_demo:
         y.append(row['crime_rate'])
         flag += 1
 y = np.asarray(y, dtype=np.float)
+new_y = np.round(y, decimals=3)
 
-plt.ylim(0,0.5)
-plt.xlim(1,77)
+plt.figure()
+plt.ylim(0, 0.5)
+plt.xlim(1, 77)
 plt.xlabel('Communities')
 plt.ylabel('Rate')
-plt.plot(x,y)
+#plt.plot(x,y)
+#plt.scatter(x, y, s=y*100, color="red", alpha=0.5)
+plt.plot(x, y, "-o")
+
+for i, txt in enumerate(new_y):
+    plt.annotate(txt, (x[i],y[i]))
+
 plt.show()
